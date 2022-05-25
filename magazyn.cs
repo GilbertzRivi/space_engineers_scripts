d14882b75af9bd36b357e21757f8a8bf110ea22d
GilbertzRivi <62563806+GilbertzRivi@users.noreply.github.com>
2022-05-25 21:24:07 +0200

    Create magazyn.cs

---
+++ magazyn.cs
@@ -0,0 +1,32 @@
+public Program(){
+    Runtime.UpdateFrequency = UpdateFrequency.Update100;
+}
+
+public void Main(string argument, UpdateType updateType){
+    IMyBlockGroup magazyn_group = GridTerminalSystem.GetBlockGroupWithName("cargo");
+    List<IMyCargoContainer> cargo = new List<IMyCargoContainer>();
+    magazyn_group.GetBlocksOfType(cargo); 
+    IMyTextSurface LCD = Me.GetSurface(0);
+    LCD.ContentType = ContentType.TEXT_AND_IMAGE;
+    LCD.FontSize = (float)0.8;
+    LCD.Alignment = VRage.Game.GUI.TextPanel.TextAlignment.CENTER;
+    LCD.TextPadding = (float)40;
+    
+    float usedVolume = 0;
+    float maxVolume = 0;
+
+    for(int i = 0; i < cargo.Count; i++){
+        var cargo_b = cargo[i];
+        usedVolume += (float)cargo_b.GetInventory(0).CurrentVolume;   
+        maxVolume += (float)cargo_b.GetInventory(0).MaxVolume;   
+    }
+    
+    int procenty = (int)Math.Round(Math.Round(usedVolume)/Math.Round(maxVolume)*40);
+    int procenty_real = (int)Math.Round(Math.Round(usedVolume)/Math.Round(maxVolume)*100);
+    string text =  new String('#', procenty);
+    text += new String('=', (40 - procenty));
+    text += "\n" + procenty_real.ToString() + "%";
+
+    LCD.WriteText(text);
+
+}
