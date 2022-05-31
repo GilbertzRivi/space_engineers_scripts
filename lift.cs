a81b11caca2444d3da8a846d2042f970f4587327
GilbertzRivi <62563806+GilbertzRivi@users.noreply.github.com>
2022-05-31 04:10:10 +0200

    Update lift.cs

--- lift.cs
+++ lift.cs
@@ -1,18 +1,27 @@
 
 public void Main(string argument){
-    IMyBlockGroup lift_grp = GridTerminalSystem.GetBlockGroupWithName("pistony winda");
+    IMyBlockGroup lift_grp = GridTerminalSystem.GetBlockGroupWithName("pistony winda miner");
     List<IMyPistonBase> pistons = new List<IMyPistonBase>();
     lift_grp.GetBlocksOfType(pistons);
+    IMyPistonBase piston_main = GridTerminalSystem.GetBlockWithName("piston winda main") as IMyPistonBase;
 
-    if(argument == "UP"){
+    if(argument == "UP miner"){
         foreach(var piston in pistons){
             piston.Velocity = (float)2;
         }
     }
 
-    else if(argument == "DOWN"){
+    else if(argument == "DOWN miner"){
         foreach(var piston in pistons){
             piston.Velocity = (float)-2;
         }
+    }
+    
+    else if(argument == "UP mine"){
+        piston_main.Velocity = (float)4;
+    }
+
+    else if(argument == "DOWN mine"){
+        piston_main.Velocity = (float)-4;
     }
 }
